0fe611caac81d738806a51fca0d9f2549825d193
井上 達也 <inoue_t@mti.co.jp>
2020-04-10 18:56:25 +0900

    add: SoundManagerで再生するように実装

--- app/src/main/java/com/example/androidpracticeforjava/MediaFragment.java
+++ app/src/main/java/com/example/androidpracticeforjava/MediaFragment.java
@@ -1,6 +1,7 @@
 package com.example.androidpracticeforjava;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,47 +15,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.androidpracticeforjava.service.SoundManagerService;
+
 import java.io.IOException;
 
 
 public class MediaFragment extends Fragment {
     private Activity _parentActivity;
 
-    private MediaPlayer _player;
     private Button _btPlay;
-    private Button _btBack;
-    private Button _btForward;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         _parentActivity = getActivity();
-
-        _player = new MediaPlayer();
-
-        String mediaFileUrlStr =
-                "android.resource://" +
-                _parentActivity.getPackageName() +
-                "/" +
-                R.raw.flexxx;
-        Uri mediaFileUri = Uri.parse(mediaFileUrlStr);
-
-        try {
-            // 音声ファイルを指定する
-            _player.setDataSource(_parentActivity, mediaFileUri);
-
-            // 再生準備が完了した時のリスナを設定
-            _player.setOnPreparedListener(new PlayerPreparedListener());
-
-            // メディア再生が終了した時のリスナを設定
-            _player.setOnCompletionListener(new PlayerCompletionListener());
-
-            _player.prepareAsync();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -68,78 +43,21 @@ public class MediaFragment extends Fragment {
         _btPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (_player.isPlaying()) {
-                    _player.pause();
-                    _btPlay.setText(R.string.bt_play_play);
-                }
-                else {
-                    _player.start();
+                Intent intent = new Intent(_parentActivity, SoundManagerService.class);
+
+                Log.d("sound", "" + _btPlay.getText());
+
+                if (_btPlay.getText().equals("再生")) {
+                    _parentActivity.startService(intent);
                     _btPlay.setText(R.string.bt_play_pause);
                 }
-            }
-        });
-
-        _btBack = view.findViewById(R.id.btBack);
-        _btBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                _player.seekTo(0);
-            }
-        });
-
-        _btForward = view.findViewById(R.id.btForward);
-        _btForward.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int alltime = _player.getDuration();
-                int current = _player.getCurrentPosition();
-
-                Log.d("media", "alltime: "+ alltime);
-                Log.d("media", "current: "+ current);
-
-                if (current + 15000 >= alltime ) {
-                    _player.seekTo(0);
-                    _player.pause();
-                    _btPlay.setText(R.string.bt_play_play);
-                }
                 else {
-                    _player.seekTo(current + 15000);
+                    _parentActivity.stopService(intent);
+                    _btPlay.setText(R.string.bt_play_play);
                 }
             }
         });
 
         return view;
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        if (_player.isPlaying()) {
-            _player.stop();
-        }
-
-        _player.release();
-        _player = null;
-    }
-
-    // メディアクラスの準備完了で呼ばれる
-    private class PlayerPreparedListener implements MediaPlayer.OnPreparedListener {
-        @Override
-        public void onPrepared(MediaPlayer mediaPlayer) {
-            // タップ可能にする
-            _btPlay.setEnabled(true);
-            _btBack.setEnabled(true);
-            _btForward.setEnabled(true);
-        }
-    }
-
-    // メディア再生終了時に呼ばれる
-    private class PlayerCompletionListener implements MediaPlayer.OnCompletionListener {
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer) {
-            // ボタンの表示変更
-            _btPlay.setText(R.string.bt_play_play);
-        }
     }
 }
--- app/src/main/java/com/example/androidpracticeforjava/service/SoundManagerService.java
+++ app/src/main/java/com/example/androidpracticeforjava/service/SoundManagerService.java
@@ -1,16 +1,121 @@
 package com.example.androidpracticeforjava.service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
 
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.example.androidpracticeforjava.MediaFragment;
+import com.example.androidpracticeforjava.R;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+
 public class SoundManagerService extends Service {
-    public SoundManagerService() {
-    }
+
+    private MediaPlayer _player;
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        _player = new MediaPlayer();
+
+        String id = "soundmanagerservice_nortification_channel";
+        String name = getString(R.string.msg_channel_name);
+
+        // 通知の重要度を標準に設定
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+        // 通知チャネル作成
+        NotificationChannel channel = new NotificationChannel(id, name, importance);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        manager.createNotificationChannel(channel);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (_player.isPlaying()) {
+            _player.stop();
+        }
+
+        _player.release();
+        _player = null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String mediaFileUrlStr =
+            "android.resource://" +
+            getPackageName() +
+            "/" +
+            R.raw.great_hope;
+        Uri mediaFileUri = Uri.parse(mediaFileUrlStr);
+
+        try {
+            // 音声ファイルを指定する
+            _player.setDataSource(SoundManagerService.this, mediaFileUri);
+
+            // 再生準備が完了した時のリスナを設定
+            _player.setOnPreparedListener(new PlayerPreparedListener());
+
+            // メディア再生が終了した時のリスナを設定
+            _player.setOnCompletionListener(new PlayerCompletionListener());
+
+            _player.prepareAsync();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return START_NOT_STICKY;
+    }
+
+
+    // メディアクラスの準備完了で呼ばれる
+    private class PlayerPreparedListener implements MediaPlayer.OnPreparedListener {
+        @Override
+        public void onPrepared(MediaPlayer mediaPlayer) {
+            mediaPlayer.start();
+        }
+    }
+
+    // メディア再生終了時に呼ばれる
+    private class PlayerCompletionListener implements MediaPlayer.OnCompletionListener {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                    SoundManagerService.this,
+                    "soundmanagerservice_nortification_channel"
+            );
+            builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+            builder.setContentTitle(getString(R.string.msg_title_finish));
+            builder.setContentText(getString(R.string.msg_text_finish));
+
+            Notification notification = builder.build();
+            NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+
+            manager.notify(0, notification);
+
+            stopSelf();
+        }
     }
 }
